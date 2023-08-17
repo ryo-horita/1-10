76645eb9386171810f4bb8a93f3015756dcb6a51
ryohorita <ryopiko2021@gmail.com>
2023-08-17 12:31:52 +0000

    1-10

---
+++ practice.rb
@@ -0,0 +1,40 @@
+puts 'hello ruby!'
+
+a = 3
+b = 7
+puts a + b
+
+
+
+array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+puts array_month[7]
+
+
+hello = "Hello, "
+name = "obunaga"
+world = "’s World!"
+puts hello + name + world
+
+omusubi_tech = "omusubi "
+omusubi_tech += "tech"
+puts omusubi_tech
+
+calendar_2023 = {
+  "January"   => "1月",
+  "February"  => "2月",
+  "March"     => "3月",
+  "April"     => "4月",
+  "May"       => "5月",
+  "June"      => "6月",
+  "July"      => "7月",
+  "August"    => "8月",
+  "September" => "9月",
+  "October"   => "10月",
+  "November"  => "11月",
+  "December"  => "12月"
+}
+
+puts calendar_2023["December"]
+
+
+
